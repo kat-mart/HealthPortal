39def0045d7f801cea5046c6514b0356a83008ef
Kelly Ung <kellyung@myyahoo.com>
2025-05-06 10:11:02 -0700

    add patientId and doctorId

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,15 +18,17 @@ import HealthRecords from './components/HealthRecords';
 
 function App() {
   const [role, setRole] = useState("");
+  const [patientId, setPatientId] = useState("")
+  const [doctorId, setDoctorId] = useState("")
 
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<LandingPage setRole={setRole} />} /> 
-        <Route path="/DoctorLogin" element={<DoctorLogin />} />
-        <Route path="/PatientLogin" element={<PatientLogin />} />
-        <Route path="/Doctor" element={<Doctor role={role} />} />
-        <Route path="/Patient" element={<Patient role={role} />} />
+        <Route path="/DoctorLogin" element={<DoctorLogin setId={setDoctorId} />} />
+        <Route path="/PatientLogin" element={<PatientLogin setId={setPatientId} />} />
+        <Route path="/Doctor" element={<Doctor role={role} id={doctorId} />} />
+        <Route path="/Patient" element={<Patient role={role} id={patientId} />} />
         <Route path="/Appointments" element={<Appointments role={role} />} />
         <Route path="/Messages" element={<Messages role={role} />} />
         <Route path="/HealthRecords" element={<HealthRecords role={role} />} />
