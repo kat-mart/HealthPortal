fb1e34aeac852ad496a2381f59bf88159cf64e6e
Kelly Ung <kellyung@myyahoo.com>
2025-05-06 10:12:52 -0700

    display patient profile

--- frontend/src/components/Patient.js
+++ frontend/src/components/Patient.js
@@ -1,36 +1,59 @@
 /*
-Home Page for Doctors.
+Home Page for Patients.
 */
 
 import './Login.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Navbar from './Navbar';
 import axios from 'axios';
 
-export default function Patient({ role }) {
-    const[patients, setPatients] = useState("");
+export default function Patient({ role, id }) {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [dob, setDob] = useState("");
+    const [gender, setGender] = useState("");
+    const [phone, setPhone] = useState("");
 
-    // call display patients API 
-    const handleDisplay = () => {
-        axios.post('http://127.0.0.1:5000/api/display-patients', {
-            role: "patient"
-        })
-        .then(res => {
-            setPatients(res.data);
-            console.log('Response from display patients server:', res.data);
-        })
-        .catch(error => {
-            console.error('Error sending message to display patients:', error);
-        });
-    };
-
+    // display patient profile
+    useEffect(() => {
+        if (id !== "") {
+          // delay the call by 1 ms
+          const timer = setTimeout(() => {
+            const fetchData = () => {
+              axios.post('http://127.0.0.1:5000/patient-profile', {
+                patient_id: id
+              })
+              .then(res => {
+                console.log('Response from patient profile server:', res.data);
+                setName(res.data.name);
+                setEmail(res.data.email);
+                setDob(res.data.dob);
+                setGender(res.data.gender);
+                setPhone(res.data.phone);
+              })
+              .catch(error => {
+                console.error('Error sending message to patient profile:', error);
+              });
+            };
+    
+            fetchData(); 
+          }, 1);  // 1ms delay
+    
+          // cleanup timeout if the component is unmounted or if id changes
+          return () => clearTimeout(timer);
+        }
+    }, [id]); // run when id changes
 
     return (
         <div className="container">
             <Navbar role={role} />
             <h1>Patient Dashboard</h1>
-            <button onClick={handleDisplay}>Click to Display Patients</button>
-            <div>{patients.patients}</div>
+            <h2>Hi, {name}</h2>
+            <p>Patient ID: {id}</p>
+            <p>Email: {email}</p>
+            <p>Date of Birth: {dob}</p>
+            <p>Gender: {gender}</p>
+            <p>Phone: {phone}</p>
         </div>
     )
 }
